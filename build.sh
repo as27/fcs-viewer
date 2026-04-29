189a45b5820e93ae61fe699a65a6b82f645ad971
Andreas Schröpfer <20220983+as27@users.noreply.github.com>
2026-04-29 06:15:43 +0200

    Build Datei angepasst, dass Version und Zielarchitektur im Dateinamen angegeben werden.

--- build.sh
+++ build.sh
@@ -5,13 +5,16 @@ VERSION=$(grep 'AppVersion' app.go | grep -o '"[^"]*"' | tr -d '"')
 echo "Version: $VERSION"
 
 echo "macOS (arm64)..."
-wails build -platform darwin/arm64 -o "fcs-viewer"
+wails build -platform darwin/arm64
+mv build/bin/fcs-viewer.app "build/bin/fcs-viewer-mac-arm64-$VERSION.app"
 
-#echo "macOS (amd64)..."
-#wails build -platform darwin/amd64 -o "fcs-viewer-mac-amd64-$VERSION"
+echo "macOS (amd64)..."
+wails build -platform darwin/amd64
+mv build/bin/fcs-viewer.app "build/bin/fcs-viewer-mac-amd64-$VERSION.app"
 
 echo "Windows (amd64)..."
-wails build -platform windows/amd64 -o "fcs-viewer.exe"
+wails build -platform windows/amd64
+mv build/bin/fcs-viewer.exe "build/bin/fcs-viewer-$VERSION.exe"
 
 echo ""
 echo "Fertig"
